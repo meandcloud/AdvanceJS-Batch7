f147619cc95e2043fd1982de99a75842038316ab
meandcloud <77702438+meandcloud@users.noreply.github.com>
2021-03-27 21:39:54 -0500

    Added Assignment2
    Added Assignment2

---
+++ TempLiterals_Assignment2/tempLiteral.js
@@ -0,0 +1,10 @@
+let coverLetter = 
+` \n This letter is to express my interest in the job posted on your
+ website for an experienced, detailed-oriented, front-end developer.
+ As you will see, I have two years of hands on experience on coding 
+ websites and application using HTML, CSS, Bootstrap, Javascript and
+ reactJS.`
+ let userName = `amarss`
+ let date = new Date()
+console.log("Coverletter "+ coverLetter +"\n userName "+userName)
+console.log(date)
